b872dfc73f3ec38b8cebf073b97daa6ba3a601bd
datnq2 <datnq2@hocmai.vn>
2022-11-26 04:32:42 +0700

    public 1 so api

--- src/routes/Dinner.Router.js
+++ src/routes/Dinner.Router.js
@@ -6,11 +6,13 @@ const { validateCreateDinner, validateUpdateDinner, validateDeleteDinner, valida
 const authMiddleware = require('../middlewares/Auth.Middleware');
 
 
-router.use(authMiddleware.checkJwtMiddleware);
+
 router.get(api.dinner.getAll, validateGetDinner(), DinnerController.getAll);
 router.get(api.dinner.getOne, DinnerController.getAll);
-router.post(api.dinner.createOne, validateCreateDinner(), DinnerController.createOne);
-router.put(api.dinner.updateOne, validateUpdateDinner(), DinnerController.updateOne);
-router.delete(api.dinner.deleteOne, validateDeleteDinner(), DinnerController.deleteOne);
+router.use('', authMiddleware.checkJwtMiddleware, (router) => {
+    router.post(api.dinner.createOne, validateCreateDinner(), DinnerController.createOne);
+    router.put(api.dinner.updateOne, validateUpdateDinner(), DinnerController.updateOne);
+    router.delete(api.dinner.deleteOne, validateDeleteDinner(), DinnerController.deleteOne);
+})
 
 module.exports = router;
--- src/routes/Recipe.Router.js
+++ src/routes/Recipe.Router.js
@@ -6,11 +6,12 @@ const api = require('../api/Routes');
 const authMiddleware = require('../middlewares/Auth.Middleware');
 const recipeRequest = require('../request/Recipe.Request');
 
-router.use(authMiddleware.checkJwtMiddleware);
 router.get(api.recipe.getAll, RecipeController.getAll);
 router.get(api.recipe.getOne, RecipeController.getOne);
-router.post(api.recipe.createOne, recipeRequest.validateCreate(), RecipeController.createOne);
-router.put(api.recipe.updateOne, recipeRequest.validateUpdate(), RecipeController.updateOne);
-router.delete(api.recipe.deleteOne, RecipeController.deleteOne);
+router.use('', authMiddleware.checkJwtMiddleware, (router) => {
+    router.post(api.recipe.createOne, recipeRequest.validateCreate(), RecipeController.createOne);
+    router.put(api.recipe.updateOne, recipeRequest.validateUpdate(), RecipeController.updateOne);
+    router.delete(api.recipe.deleteOne, RecipeController.deleteOne);
+})
 
 module.exports = router;
